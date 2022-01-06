207551310c2d9786fb86f822717a8f84c9a8743f
Yazganptr <82478281+Yazganptr@users.noreply.github.com>
2022-01-06 06:34:51 +0300

    github trial 2

--- WebProject/Controllers/HomeController.cs
+++ WebProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page....";
 
             return View();
         }
